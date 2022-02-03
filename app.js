8d25124790594116f486f2efaece04762e511434
AndreaGir <girolamiandrea2000@gmail.com>
2022-02-04 00:44:01 +0100

    prompt + html

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+let primaPartePassword = prompt( ' Come ti chiami? ' )
+
+password.innerHTML = primaPartePassword
+
+let secondaPartePassword = prompt( ' Quale è il tuo cognome? ' )
+console.log(prompt)
+
+password.innerHTML = primaPartePassword + secondaPartePassword
+
+let terzaPartePassword = prompt( ' Quale è il tuo colore preferito? ' )
+console.log(prompt)
+
+let datiUser = primaPartePassword + secondaPartePassword + terzaPartePassword
+
+password.innerHTML = datiUser
+
